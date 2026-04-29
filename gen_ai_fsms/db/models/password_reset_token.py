c2c72189abc80413dabe824df39c12813571f9ea
JoseWongg <j.wong@mail.com>
2026-04-29 21:20:52 +0100

    fix(auth): add Boolean import and generate password reset token migration

--- gen_ai_fsms/db/models/password_reset_token.py
+++ gen_ai_fsms/db/models/password_reset_token.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.sql import func
 from gen_ai_fsms.db.base import Base
 
